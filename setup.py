bad3ff78a12e1c5cc6bb7ceeb8e70a117f5769a3
diorge <diorge.bs@gmail.com>
2016-04-29 16:52:43 -0300

    Project sketch

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+setup(
+    name = 'cross',
+    version = '0.1',
+    author = 'diorge',
+    author_email = 'diorge.bs@gmail.com',
+    description = 'Chess variant',
+    url = 'http://github.com/diorge/game-cross',
+    license = 'MIT License',
+    keywords = 'game',
+    packages = ['cross'],
+    long_description = read('README.md'),
+
+    test_suite = 'cross.test.testsuite',
+    tests_require = ['pytest'],
+    classifiers = [
+        'Programming Language :: Python',
+        'Development Status :: 2 - Pre-Alpha',
+        'Natural Language :: English',
+        'Environment :: Console',
+        'Environment :: Console Curses',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Games/Entertainment'
+    ],
+    extras_require = {
+        'testing' : ['pytest']
+    },
+    install_requires = [
+        'wheel>=0.24.0'
+    ]
+)
