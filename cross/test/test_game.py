3ad20238207c84dde7c7d3a8101c6590e94bc31e
diorge <diorge.bs@gmail.com>
2016-05-01 10:28:57 -0300

    Test for starting board

--- cross/test/test_game.py
+++ cross/test/test_game.py
@@ -9,3 +9,17 @@ class TestGame(TestCase):
         p = cgame.Piece(cgame.TroopType.Spearman, cgame.Color.White)
         assert p.trooptype == cgame.TroopType.Spearman
         assert p.color == cgame.Color.White
+
+    def test_starting_board(self):
+        """ Tests the validity of the starting board """
+        b = cgame.new_board()
+        sp, sw, a, h, g = cgame.TroopType.__iter__()
+        b, w = cgame.Color.__iter__()
+        p = cgame.Piece
+        expected = cgame.Board([
+            [p(sp, w), p(sw, w), p(sw, w), p(h, w), p(g, w), p(h, w), p(sw, w), p(sw, w), p(sp, w)],
+            [p(sp, w), p(sp, w), p(sw, w), p(a, w), p(a, w), p(a, w), p(sw, w), p(sp, w), p(sp, w)],
+            [], [], [], [], [],
+            [p(sp, b), p(sp, b), p(sw, b), p(a, b), p(a, b), p(a, b), p(sw, b), p(sp, b), p(sp, b)],
+            [p(sp, b), p(sw, b), p(sw, b), p(h, b), p(g, b), p(h, b), p(sw, b), p(sw, b), p(sp, b)]])
+        assert expected == b
