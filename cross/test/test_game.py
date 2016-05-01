19a74c53a21f2e52dc879acf5901784ddc2363a9
diorge <diorge.bs@gmail.com>
2016-05-01 10:45:52 -0300

    Test for position to index

--- cross/test/test_game.py
+++ cross/test/test_game.py
@@ -10,6 +10,15 @@ class TestGame(TestCase):
         assert p.trooptype == cgame.TroopType.Spearman
         assert p.color == cgame.Color.White
 
+    def test_position_index(self):
+        """ Tests conversion of (H, V) coordinate to 0-based index """
+        zero = cgame.Position(1, 'A')
+        idx = cgame.Position(4, 'C')
+        final = cgame.Position(9, 'I')
+        assert 0 == zero
+        assert 22 == idx
+        assert 80 == final
+
     def test_starting_board(self):
         """ Tests the validity of the starting board """
         b = cgame.new_board()
